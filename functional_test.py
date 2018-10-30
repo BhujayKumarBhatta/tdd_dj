db4e36c6bf4b05d79835bc279728ce92a68bf221
bhujay <bhujay.bhatta@yahoo.com>
2018-10-30 05:26:21 -0700

    selenium driver was added

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
 #driverLocation = "/mnt/c/mydev/tdd_dj/chromedriver"
 #driverLocation = "/mnt/c/mydev/tdd_dj"
+#driverLocation = "/mnt/c/mydev/tdd_dj"  , not required when driver is in path 
+# the driver for firefox is here 
+# https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux32.tar.gz
+#https://github.com/SeleniumHQ/selenium/blob/master/py/docs/source/index.rst
+#
 browser = webdriver.Firefox()
 
 # sumanta goes to check his new website
